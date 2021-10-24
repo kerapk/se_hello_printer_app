6b79d98ccd2dbbf71dbc160b601b838ab08fe16c
kerapk <kerapk@users.noreply.github.com>
2021-10-24 07:06:28 +0000

    Moje zmiany

--- hello_world/views.py
+++ hello_world/views.py
@@ -19,3 +19,8 @@ def index():
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
+
+
+output_name = request.args.get('name')
+if not (output_name != Null):
+moje_imie = output_name
